d7a3158d2b1d07324d1fcbafad1a1ece65f39900
Shreyansh-77 <shreyanshsoni9136@gmail.com>
2024-08-22 20:50:08 +0530

    About section context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import About from "./components/About.jsx";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero />
+        <Hero />
+        <About />
     </main>
   );
 }
