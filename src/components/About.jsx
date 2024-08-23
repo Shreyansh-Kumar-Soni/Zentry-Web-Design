6309028df1ce470b87458e81bacf4cf922f8fcf3
Shreyansh-77 <shreyanshsoni9136@gmail.com>
2024-08-23 21:50:08 +0530

    Added Animated titles component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
-import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
-import {ScrollTrigger} from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
     useGSAP(() => {
@@ -13,7 +17,7 @@ const About = () => {
                 pin: true,
                 pinSpacing: true,
             },
-        })
+        });
 
         clipAnimation.to(".mask-clip-path", {
             width: "100vw",
@@ -22,35 +26,38 @@ const About = () => {
         });
     });
 
-        return (
-        <div id="about" className={"min-h-screen w-screen"}>
-            <div className={"relative mb-8 mt-36 flex flex-col items-center gap-5"}>
-                <h2 className={"font-general text-sm uppercase md:text-[10px]"}>
+    return (
+        <div id="about" className="min-h-screen w-screen">
+            <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+                <p className="font-general text-sm uppercase md:text-[10px]">
                     Welcome to Zentry
-                </h2>
+                </p>
 
-                <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                    Disc<b>o</b>ver the worlds <b>L</b>argest shared adventure
-                </div>
+                <AnimatedTitle
+                    title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+                    containerClass="mt-5 !text-black text-center"
+                />
 
-                <div className={"about-subtext"}>
-                    <p> The Game of Games begins-your life, now an epic MMORPG</p>
-                    <p>
-                        Zentry unites every player from countless games and platform
+                <div className="about-subtext">
+                    <p>The Game of Games begins—your life, now an epic MMORPG</p>
+                    <p className="text-gray-500">
+                        Zentry unites every player from countless games and platforms, both
+                        digital and physical, into a unified Play Economy
                     </p>
                 </div>
             </div>
 
-            <div className={"h-dvh w-screen"}  id="clip">
+            <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path about-image">
                     <img
                         src="img/about.webp"
-                        alt="Backgrond"
-                        className="absolute top-0 left-0 size-full object-cover"
+                        alt="Background"
+                        className="absolute left-0 top-0 size-full object-cover"
                     />
                 </div>
             </div>
         </div>
-    )
-}
-export default About
+    );
+};
+
+export default About;
