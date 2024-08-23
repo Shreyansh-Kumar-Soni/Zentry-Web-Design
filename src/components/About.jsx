0f92d850ef8c02752755e978ee2e1835d9860a44
Shreyansh-77 <shreyanshsoni9136@gmail.com>
2024-08-23 20:50:08 +0530

    Added about section animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,28 @@
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/all";
+
 const About = () => {
-    return (
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            },
+        })
+
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0,
+        });
+    });
+
+        return (
         <div id="about" className={"min-h-screen w-screen"}>
             <div className={"relative mb-8 mt-36 flex flex-col items-center gap-5"}>
                 <h2 className={"font-general text-sm uppercase md:text-[10px]"}>
